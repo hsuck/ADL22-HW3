82fca175c317b6148e99858d7745b307c547a9b7
hsuck <xmjj726716@gmail.com>
2022-11-23 15:02:46 +0800

    upload HW

---
+++ run.sh
@@ -0,0 +1,12 @@
+python run_summarization.py \
+    --model_name_or_path mt5-small/greedy \
+    --do_predict \
+    --test_file ${1} \
+    --predict_with_generate \
+    --text_column maintext \
+    --per_device_eval_batch_size 4 \
+    --num_beams 4 \
+    --do_sample \
+    --top_p 0.6 \
+    --output_file ${2} \
+    --output_dir ./output/result \
