6aafaebc3f75bb8405dc7043fc346ba37bab2ff6
hsuck <xmjj726716@gmail.com>
2022-11-27 23:29:22 +0800

    upload the scripts for downloading model

--- download.py
+++ download.py
@@ -1,4 +1,4 @@
 import gdown
 
-url = "https://drive.google.com/u/1/uc?id=1qM-fuGCBLvFj73SQ3zQPiqjrvgQZhg4n&export=download"
+url = "https://drive.google.com/u/1/uc?id=1qM-fuGCBLvFj73SQ3zQPiqjrvgQZhg4n"
 gdown.download( url, "mt5-small.zip" )
